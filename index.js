d579a7b070d445634d86c1bc4445a77d174a9c76
SQLAdmin <abuyeahia24@gmail.com>
2023-04-22 03:32:26 +0600

    added news data for server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 
 const categories = require('./data/categories.json')
-
+const news = require('./data/news.json')
 app.use(cors());
 
 app.get('/', (req, res) => {
@@ -13,8 +13,30 @@ app.get('/', (req, res) => {
 })
 
 // data
-app.get('/categories', (req,res) =>{
-    res.send(categories)
+app.get('/categories', (req, res) => {
+  res.send(categories);
+})
+
+app.get('/categories/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id === 0) {
+    res.send(news)
+  }
+  else {
+    const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+    res.send(categoryNews)
+  }
+
+})
+
+
+
+// news section
+app.get('/news/:id', (req, res) => {
+  // res.send(news);
+  const id = req.params.id;
+  const selectedNews = news.find(n => n._id === id);
+  res.send(selectedNews)
 })
 
 app.listen(port, () => {
